3db8f2d164e8d050917d8ac3eb0ebe29edde3127
ROSA-HP <fareshassan437@gmail.com>
2026-02-23 12:18:07 +0200

    Add flow rate sensor functionality and pulse counting logic

--- esp32-code/src/main.cpp
+++ esp32-code/src/main.cpp
@@ -3,8 +3,20 @@
 
 int increasePin = 12;
 int decreasePin = 14;
+const int sensorPin = 36; 
+
+// Variables for pulse counting
+volatile long pulseCount = 0;
+float flowRate = 0.0;
+unsigned int flowMilliLitres = 0;
+unsigned long totalMilliLitres = 0;
+unsigned long oldTime = 0;
+
 // float temp = 15;
 
+void IRAM_ATTR pulseCounter() {
+  pulseCount++;
+}
 void increaseTemp();
 void decreaseTemp();
 bool inBetween(float x, float minimum, float maximum);
@@ -20,6 +32,10 @@ void setup() {
   pinMode(increasePin, OUTPUT);
   pinMode(decreasePin, OUTPUT);
   dht.begin();
+
+  pinMode(sensorPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(sensorPin), pulseCounter, FALLING);
+  oldTime = millis();
   delay(2000);
   Serial.begin(115200);
 }
@@ -31,6 +47,7 @@ void loop() {
   // if (sensorState) temp -= 0.5;
   // else temp += 0.5;
   
+  //Temp & Humidity sensor
   float temp = dht.readTemperature();
   float humidity = dht.readHumidity();
 
@@ -40,10 +57,41 @@ void loop() {
   Serial.print(humidity);
   Serial.println(" %");
   temperatureState = actuate(temp, 23, 1, 3, increaseTemp, decreaseTemp, temperatureState, 0);
-  Serial.println(temp, !temperatureState);
   delay(2000);
   digitalWrite(increasePin, LOW);
   digitalWrite(decreasePin, LOW);
+
+  //Flow rate sensor
+    // Calculate flow every 1 second
+  if ((millis() - oldTime) > 1000) {
+    
+    // Disable interrupts while reading/resetting pulseCount to avoid data corruption
+    detachInterrupt(digitalPinToInterrupt(sensorPin));
+    
+    // The YF-S201 characteristic: Frequency (Hz) = 7.5 * Q (L/min)
+    // Q = pulses / 7.5
+    flowRate = (pulseCount / 7.5); 
+    
+    oldTime = millis();
+    
+    // Calculate volume passed in this second
+    flowMilliLitres = (flowRate / 60) * 1000;
+    totalMilliLitres += flowMilliLitres;
+
+    // Print results
+    Serial.print("Flow rate: ");
+    Serial.print(flowRate);
+    Serial.print(" L/min");
+    Serial.print("\t Total: ");
+    Serial.print(totalMilliLitres);
+    Serial.println(" mL");
+
+    // Reset pulse count for the next second
+    pulseCount = 0;
+    
+    // Re-enable interrupts
+    attachInterrupt(digitalPinToInterrupt(sensorPin), pulseCounter, FALLING);
+}
 }
 
 void increaseTemp() {
